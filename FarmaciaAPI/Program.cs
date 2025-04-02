de548e88a1ce1a72d3355b176f2f8438957684d0
Rafaela <rafaelabaptista464@gmail.com>
2025-04-02 20:54:34 -0300

    teste

--- FarmaciaAPI/Program.cs
+++ FarmaciaAPI/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
+//teste
 
 //builder.Services.AddAuthentication(JwtBearerDefauts.AuthenticationScheme).AddBearerToken(options =>
 //    {
